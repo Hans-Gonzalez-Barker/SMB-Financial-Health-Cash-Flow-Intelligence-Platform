1db994cb2e7b0b711b81c2c005a1ec12f8f65b52
Hans-Gonzalez-Barker <hansgonzalez1216@gmail.com>
2026-06-22 13:53:00 -0400

    feat: added Neon DB test queries to databse folder

--- database/test_queries.sql
+++ database/test_queries.sql
@@ -15,3 +15,8 @@ SELECT
     company_id, 
     jsonb_pretty(payload) AS formatted_payload 
 FROM raw_stage.plaid_transactions;
+
+-- Test ingestion script against Neon Cloud DB
+SELECT 'plaid_transactions' AS table_name, COUNT(*) FROM raw_stage.plaid_transactions UNION ALL
+SELECT 'quickbooks_invoices' AS table_name, COUNT(*) FROM raw_stage.quickbooks_invoices UNION ALL
+SELECT 'quickbooks_payments' AS table_name, COUNT(*) FROM raw_stage.quickbooks_payments;
